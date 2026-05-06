68de58519a19e4207e1d4f5bed6f49b865e91679
PedroGSantos <pedrogsantos_10@hotmail.com>
2026-05-06 19:27:36 -0300

    feat: new easy exercise

---
+++ Exercícios-LeetCode/Valid Anagram/solution.ts
@@ -0,0 +1,27 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length){
+        return false
+    }
+
+    let map = new Map()
+
+    for (let i = 0; i < s.length; i++){
+        if (map.has(s[i])){
+            map.set(s[i], map.get(s[i]) + 1)
+        } else {
+            map.set(s[i], 1)
+        }
+    }
+
+    for (let i = 0; i < s.length; i++){
+        if (map.has(t[i]))
+            map.set(t[i], map.get(t[i]) - 1)
+    }
+
+    for (let i = 0; i < s.length; i++){
+        if (map.get(s[i]) > 0)
+            return false
+    }
+
+    return true
+};
